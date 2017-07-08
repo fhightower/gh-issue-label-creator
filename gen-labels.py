cc9d3a8a8e88de547c5359da0c442258dc7a641a
fhightower <floyd.hightower27@gmail.com>
2017-07-08 16:47:36 -0300

    Added ability to add labels to all repos for a given owner

--- gen-labels.py
+++ gen-labels.py
@@ -21,8 +21,9 @@ def setup_args_parser():
                         help="github password, or application token for 2FA")
     parser.add_argument('-o', '--owner', dest='owner', required=True,
                         help="the owner of the repository to update")
-    parser.add_argument('-r', '--repo', dest='repository', required=True,
+    parser.add_argument('-r', '--repo', dest='repository',
                         help="the repository to update")
+    parser.add_argument('--all', action='store_true')
     parser.add_argument('-d', '--def', dest='definitions',
                         help="location of json file containing label " +
                              "definitions. Defaults to definitions.json",
@@ -44,11 +45,31 @@ def read_definitions(file_):
         return json.load(stream)
 
 
-def generate_request(args, label_def):
+def create_request(args, label_def, repo_name=None):
+    if repo_name is None:
+        repo_name = args.repository
+
     url = "https://api.github.com/repos/{}/{}/labels".format(args.owner,
-                                                             args.repository)
+                                                             repo_name)
     body = json.dumps({'name': label_def['name'], 'color': label_def['color']})
     return (url, body)
+
+
+def make_request(args, label=None, request_url=None, repo_name=None):
+    auth = (args.username, args.password)
+    header = {
+        'X-GitHub-OTP': str(two_factor)
+    }
+    if request_url is None:
+        http_request = create_request(args, label, repo_name)
+        request_url = http_request[0]
+        response = requests.post(request_url, headers=header,
+                                 data=http_request[1], auth=auth, timeout=10)
+    else:
+        response = requests.get(request_url, headers=header, auth=auth,
+                                 timeout=10)
+
+    return response, request_url
 
 
 def print_progress(name, color, request):
@@ -59,35 +80,67 @@ def test_output(args, label_defs):
     output("This will generate the following labels, using HTTP requests:")
     for label_def in label_defs['label']:
         print_progress(label_def['name'], label_def['color'],
-                       generate_request(args, label_def)[0])
+                       create_request(args, label_def)[0])
+
+
+def get_all_repos(args):
+    """Get all repos for a user."""
+    all_repos = list()
+    all_repos_url = 'https://api.github.com/users/{}/repos'.format(args.owner)
+
+    # get a list of all the repositories for the owner
+    response, request_url = make_request(args, request_url=all_repos_url)
+
+    if response.ok:
+        repos = json.loads(response.text)
+        for repo in repos:
+            all_repos.append(repo['name'])
+    else:
+        raise RuntimeError("Unable to get a list of repositories for the " +
+                           "owner using the url: {}".format(request_url))
+
+    return all_repos
 
 
 def issue_requests(args, label_defs):
-    two_factor = input("Enter two factor code: ")
-    output("Creating labels:")
+    # populate a list of the repos we would like to update
+    repositories = list()
+
+    if args.all is not None:
+        repositories.extend(get_all_repos(args))
+    elif args.repository is not None:
+        repositories.append(args.repository)
+
+    print(repositories)
+
+    if len(repositories) > 0:
+        print("Creating labels:")
+
     for label_def in label_defs['label']:
-        output("  creating '{}' with color '{}'".format(label_def['name'],
-                                                        label_def['color']))
+        for repo in repositories:
+            response, request_url = make_request(args, label=label_def, repo_name=repo)
 
-        auth = (args.username, args.password)
-        http_request = generate_request(args, label_def)
-        header = {
-            'X-GitHub-OTP': str(two_factor)
-        }
-        response = requests.post(http_request[0], headers=header,
-                                 data=http_request[1], auth=auth, timeout=10)
-
-        if (response.status_code != 200 and response.status_code != 201):
-            output("  failed: (%s) %s" % (response.status_code, response.text))
-        else:
-            output("  done (%s)" % (response.status_code))
+            if (response.status_code != 200 and response.status_code != 201):
+                print("  failed request to {}: ({}) {}".format(request_url, response.status_code, response.text))
+            else:
+                output("  done (%s)" % (response.status_code))
 
 
 if __name__ == '__main__':
     args = parse_args()
+
+    # check to see if either 'all' is set to True (meaning we will add these)
+    # labels for all of the owner's repos or -r is set specifying the specific
+    # repo to which the labels will be added
+    if not args.all and args.repository is None:
+        raise RuntimeError("You need to either specify a repo using the -r " +
+                           "flag or use the --all flag to add the labels to " +
+                           "all repos for the current owner.")
+
     label_defs = read_definitions(args.definitions)
 
     if args.test:
         test_output(args, label_defs)
     else:
+        two_factor = input("Enter two factor code: ")
         issue_requests(args, label_defs)
